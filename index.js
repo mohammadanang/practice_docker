b49f3c4365fc1f0913947c452da2857c469c9fa3
mohammadanang <m.anangnur@gmail.com>
2019-01-29 17:01:42 +0700

    fix error connecting to postgres

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const server = express();
 const PORT = 3000;
 const pg = require('pg');
-pg.connect('postgres://postgres:kiasu123@localhost:5432/practice_docker');
+
+var config = {
+    user: 'postgres',
+    database: 'practice_docker',
+    password: 'password',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+};
+var pool = new pg.Pool(config);
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 server.get('/', (req, res) => res.status(200).send('Hello'));
+
+server.get('/pool', (req, res) => {
+    pool.connect((err, client, done) => {
+        if (err) {
+            res.status(400).send(err);
+        }
+
+        res.status(200).send('Connect to postgres');
+    });
+});
